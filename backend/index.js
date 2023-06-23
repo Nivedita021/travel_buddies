52079fa1776b93493633119c84823147e7d01fe1
Nivedita Chatterjee <99239032+Nivedita021@users.noreply.github.com>
2023-06-23 21:57:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("MongoDB Connected!");
 })
+    app.get('/', (req, res) => {
+    res.end('it works!');
+});
 .catch((err) => console.log(err));
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
